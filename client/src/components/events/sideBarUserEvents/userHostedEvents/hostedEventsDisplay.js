3e05ccfe56242d26c67cebad9684bee9e06c268d
Nicolas Toulemont <nicolahs.toulemont@gmail.com>
2019-03-01 16:47:07 +0100

    0 events or registrations msg

--- client/src/components/events/sideBarUserEvents/userHostedEvents/hostedEventsDisplay.js
+++ client/src/components/events/sideBarUserEvents/userHostedEvents/hostedEventsDisplay.js
@@ -2,38 +2,48 @@ import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 const HostedEventsDisplay = ({ events }) => {
-  return (
-    <Fragment>
-      {events.map(event => {
-        return (
-          <div className="text-left px-3 py-2 border-top" key={event.id}>
-            <Link
-              to={{
-                pathname: `/event/${event.id}`
-              }}
-              className="link-menu"
-            >
-              <small className="font-weight-bold mr-2 link-menu">
-                {event.name}
-              </small>
-            </Link>
-            <small className="d-block">{event.location}</small>
-            {event.startDate === event.endDate ? (
-              <small className="d-block">
-                {new Date(event.startDate).toDateString()} from{' '}
-                {event.startTime} to {event.endTime}
-              </small>
-            ) : (
-              <small className="d-block">
-                On {event.startDate} from {event.startTime} to {event.endTime}{' '}
-                on {event.endDate}
-              </small>
-            )}
-          </div>
-        );
-      })}
-    </Fragment>
-  );
+  if (events.length === 0) {
+    return (
+      <Fragment>
+        <div className="text-left pl-4 ml-1">
+          <small>No events yet</small>
+        </div>
+      </Fragment>
+    );
+  } else {
+    return (
+      <Fragment>
+        {events.map(event => {
+          return (
+            <div className="text-left px-3 py-2 border-top" key={event.id}>
+              <Link
+                to={{
+                  pathname: `/event/${event.id}`
+                }}
+                className="link-menu"
+              >
+                <small className="font-weight-bold mr-2 link-menu">
+                  {event.name}
+                </small>
+              </Link>
+              <small className="d-block">{event.location}</small>
+              {event.startDate === event.endDate ? (
+                <small className="d-block">
+                  {new Date(event.startDate).toDateString()} from{' '}
+                  {event.startTime} to {event.endTime}
+                </small>
+              ) : (
+                <small className="d-block">
+                  On {event.startDate} from {event.startTime} to {event.endTime}{' '}
+                  on {event.endDate}
+                </small>
+              )}
+            </div>
+          );
+        })}
+      </Fragment>
+    );
+  }
 };
 
 export default HostedEventsDisplay;
--- client/src/components/events/sideBarUserEvents/userRegistrations/registrationsDisplay.js
+++ client/src/components/events/sideBarUserEvents/userRegistrations/registrationsDisplay.js
@@ -2,39 +2,52 @@ import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 const RegistrationsDisplay = ({ registrations }) => {
-  return (
-    <Fragment>
-      {registrations.map(registration => {
-        return (
-          <div className="text-left px-3 py-2 border-top" key={registration.id}>
-            <Link
-              to={{
-                pathname: `/event/${registration.event.id}`
-              }}
-              className="link-menu"
+  if (registrations.length === 0) {
+    return (
+      <Fragment>
+        <div className="text-left pl-4 ml-1">
+          <small>No registrations yet</small>
+        </div>
+      </Fragment>
+    );
+  } else {
+    return (
+      <Fragment>
+        {registrations.map(registration => {
+          return (
+            <div
+              className="text-left px-3 py-2 border-top"
+              key={registration.id}
             >
-              <small className="font-weight-bold mr-2 link-menu">
-                {registration.event.name}
-              </small>
-            </Link>
-            <small className="d-block">{registration.event.location}</small>
-            {registration.event.startDate === registration.event.endDate ? (
-              <small className="d-block">
-                {new Date(registration.event.startDate).toDateString()} from{' '}
-                {registration.event.startTime} to {registration.event.endTime}
-              </small>
-            ) : (
-              <small className="d-block">
-                On {registration.event.startDate} from{' '}
-                {registration.event.startTime} to {registration.event.endTime}{' '}
-                on {registration.event.endDate}
-              </small>
-            )}
-          </div>
-        );
-      })}
-    </Fragment>
-  );
+              <Link
+                to={{
+                  pathname: `/event/${registration.event.id}`
+                }}
+                className="link-menu"
+              >
+                <small className="font-weight-bold mr-2 link-menu">
+                  {registration.event.name}
+                </small>
+              </Link>
+              <small className="d-block">{registration.event.location}</small>
+              {registration.event.startDate === registration.event.endDate ? (
+                <small className="d-block">
+                  {new Date(registration.event.startDate).toDateString()} from{' '}
+                  {registration.event.startTime} to {registration.event.endTime}
+                </small>
+              ) : (
+                <small className="d-block">
+                  On {registration.event.startDate} from{' '}
+                  {registration.event.startTime} to {registration.event.endTime}{' '}
+                  on {registration.event.endDate}
+                </small>
+              )}
+            </div>
+          );
+        })}
+      </Fragment>
+    );
+  }
 };
 
 export default RegistrationsDisplay;
