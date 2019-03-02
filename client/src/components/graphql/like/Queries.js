ca89ff663187e7c432563b48d2527bae6c13fc14
Nicolas Toulemont <nicolahs.toulemont@gmail.com>
2019-03-02 16:38:44 +0100

    revert to first changes to client graphql folder using the gql tag from graphql-tag

--- client/src/components/graphql/like/Queries.js
+++ client/src/components/graphql/like/Queries.js
@@ -1,4 +1,4 @@
-import { gql } from 'apollo-boost';
+import gql from 'graphql-tag';
 
 export const GET_EVENT_LIKES = gql`
   query Event($id: ID!) {
--- client/src/components/graphql/profile/Queries.js
+++ client/src/components/graphql/profile/Queries.js
@@ -1,4 +1,4 @@
-import { gql } from 'apollo-boost';
+import gql from 'graphql-tag';
 
 export const GET_USER_FULL_PROFILE = gql`
   query User($id: ID!) {
