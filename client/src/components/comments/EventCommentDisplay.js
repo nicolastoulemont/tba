7a5f8f196a4e1793d46eb1093b8a02398de59cec
Nicolas Toulemont <nicolahs.toulemont@gmail.com>
2019-03-24 15:58:00 +0100

    refactored commentitem moderation handling and display

--- client/src/components/comments/EventCommentDisplay.js
+++ client/src/components/comments/EventCommentDisplay.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { Mutation } from 'react-apollo';
 import { MODERATE_COMMENT } from '../graphql/comment/Mutations';
@@ -21,14 +21,11 @@ const EventCommentDisplay = ({
 	moderationMsg
 }) => {
 	return (
-		<div className="list-group-item border-0 py-1 px-2" key={id}>
-			<div className="row">
-				<div className="col-1">
-					{moderated ? null : <RespSmallAvatarLink id={creatorId} avatar={creatorAvatar} />}
-				</div>
-				{moderated ? (
-					<div className="col-9 col-md-10 mx-0 pr-0 pl-1 py-0">
-						<div className="text-left mx-auto">
+		<Fragment>
+			{moderated ? (
+				<div className="list-group-item border-0 py-1 px-2" key={id}>
+					<div className="text-left mx-auto">
+						<small>
 							<small className="font-italic">{'[ deleted ]'}</small>
 							<small className="d-none d-md-inline-block font-italic ml-2 py-0">
 								{moderationMsg}
@@ -36,59 +33,65 @@ const EventCommentDisplay = ({
 							<small className="d-inline-block d-md-none font-italic ml-4 py-0">
 								{moderationMsg}
 							</small>
-						</div>
+						</small>
 					</div>
-				) : (
-					<div className="col-9 col-md-10 mx-0 pr-0 pl-1">
-						<div className="text-left mx-auto">
-							<UserNameLink id={creatorId} name={creatorName} />
-							<span className="d-none d-md-inline-block ml-2">{text}</span>
-							<span className="d-inline-block d-md-none ml-4">{text}</span>
-							<EventCommentActions
-								user={user}
-								creatorId={creatorId}
-								comment_ID={id}
-								commentText={text}
-								createdAt={createdAt}
-								updatedAt={updatedAt}
-								refetch={refetch}
-							/>
-						</div>
-					</div>
-				)}
-
-				<div className="col-1 mx-0">
-					{!moderated && (user === creatorId || user === eventCreator) ? (
-						<Mutation mutation={MODERATE_COMMENT}>
-							{(moderateComment, e) => (
-								<Link
-									to="#"
-									className="m-0 p-0 text-right"
-									onClick={e => {
-										e.preventDefault();
-										moderateComment({
-											variables: {
-												_id: id,
-												user_ID: user,
-												event_ID
-											}
-										}).then(res => {
-											refetch();
-										});
-									}}
-								>
-									{user === creatorId ? (
-										<i className="fa fa-times mx-0" aria-hidden="true" />
-									) : user === eventCreator ? (
-										<i className="fas fa-ban mx-0" aria-hidden="true" />
-									) : null}
-								</Link>
-							)}
-						</Mutation>
-					) : null}
 				</div>
-			</div>
-		</div>
+			) : (
+				<div className="list-group-item border-0 py-1 px-2" key={id}>
+					<div className="row">
+						<div className="col-1">
+							<RespSmallAvatarLink id={creatorId} avatar={creatorAvatar} />
+						</div>
+						<div className="col-9 col-md-10 mx-0 pr-0 pl-1">
+							<div className="text-left mx-auto">
+								<UserNameLink id={creatorId} name={creatorName} />
+								<span className="d-none d-md-inline-block ml-2">{text}</span>
+								<span className="d-inline-block d-md-none ml-4">{text}</span>
+								<EventCommentActions
+									user={user}
+									creatorId={creatorId}
+									comment_ID={id}
+									commentText={text}
+									createdAt={createdAt}
+									updatedAt={updatedAt}
+									refetch={refetch}
+								/>
+							</div>
+						</div>
+						<div className="col-1 mx-0">
+							{user === creatorId || user === eventCreator ? (
+								<Mutation mutation={MODERATE_COMMENT}>
+									{(moderateComment, e) => (
+										<Link
+											to="#"
+											className="m-0 p-0 text-right"
+											onClick={e => {
+												e.preventDefault();
+												moderateComment({
+													variables: {
+														_id: id,
+														user_ID: user,
+														event_ID
+													}
+												}).then(res => {
+													refetch();
+												});
+											}}
+										>
+											{user === creatorId ? (
+												<i className="fa fa-times mx-0" aria-hidden="true" />
+											) : user === eventCreator ? (
+												<i className="fas fa-ban mx-0" aria-hidden="true" />
+											) : null}
+										</Link>
+									)}
+								</Mutation>
+							) : null}
+						</div>
+					</div>
+				</div>
+			)}
+		</Fragment>
 	);
 };
 
