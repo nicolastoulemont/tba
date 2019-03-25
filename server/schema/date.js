db4e40affcd6c0b797959c3d8adfe0477e450522
Nicolas Toulemont <nicolahs.toulemont@gmail.com>
2019-03-25 11:05:58 +0100

    revert to using dayjs in the date scalar which seems to work fine now

--- server/schema/date.js
+++ server/schema/date.js
@@ -12,12 +12,11 @@ module.exports = {
 			name: 'Date',
 			description: 'Date scalar type',
 			parseValue(value) {
-				return new Date(value); // value from the client
+				return dayjs(value); // value from the client
 			},
 			serialize(value) {
-				const date = new Date(value);
-				date.setHours(date.getHours() + 1);
-				return date.toISOString().slice(0, 19); // value sent to the client
+				return dayjs(value).format('YYYY-MM-DDTHH:mm:ss');
+				// value sent to the client
 			},
 			parseLiteral(ast) {
 				if (ast.kind === Kind.STRING) {
