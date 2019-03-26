ae5e88a460cdbb0c9c4a6754966083295b347125
Nicolas Toulemont <nicolahs.toulemont@gmail.com>
2019-03-26 23:38:00 +0100

    refactored the event comment input field and added the possibilty to submit comments with the enter key

--- client/src/components/comments/EventCommentFeedInput.js
+++ client/src/components/comments/EventCommentFeedInput.js
@@ -5,65 +5,69 @@ import { ADD_COMMENT } from '../graphql/comment/Mutations';
 import { GET_EVENT_COMMENTS } from '../graphql/comment/Queries';
 
 class EventCommentFeedInput extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: ''
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			text: ''
+		};
+	}
 
-  onChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
+	onChange = e => {
+		const { name, value } = e.target;
+		this.setState({ [name]: value });
+	};
 
-  render() {
-    const { user, event_ID, refetch } = this.props;
-    const { text } = this.state;
-    return (
-      <Fragment>
-        <Mutation
-          mutation={ADD_COMMENT}
-          refetchQueries={() => {
-            return [{ query: GET_EVENT_COMMENTS, variables: { id: event_ID } }];
-          }}
-        >
-          {(addComment, e) => (
-            <div className="input-group input-group-sm py-2 px-4">
-              <input
-                type="text"
-                className="form-control mx-0"
-                placeholder="Comment..."
-                onChange={this.onChange}
-                name="text"
-                value={text}
-              />
-              <div className="input-group-append">
-                <Link
-                  to="#"
-                  className="btn bg-darkblue"
-                  onClick={e => {
-                    e.preventDefault();
-                    addComment({
-                      variables: { user_ID: user, event_ID, text }
-                    }).then(res => {
-                      refetch();
-                      this.setState({ text: '' });
-                    });
-                  }}
-                >
-                  <i
-                    className="fa fa-paper-plane text-white"
-                    aria-hidden="true"
-                  />
-                </Link>
-              </div>
-            </div>
-          )}
-        </Mutation>
-      </Fragment>
-    );
-  }
+	createComment = (e, user, event_ID, text, addComment) => {
+		if (
+			(e.type === 'click' && e.target.className === 'fa fa-paper-plane text-white') ||
+			(e.type === 'keydown' && e.keyCode === 13)
+		) {
+			e.preventDefault();
+			addComment({
+				variables: { user_ID: user, event_ID, text }
+			}).then(res => {
+				this.setState({ text: '' });
+			});
+		}
+	};
+
+	render() {
+		const { user, event_ID } = this.props;
+		const { text } = this.state;
+		return (
+			<Fragment>
+				<Mutation
+					mutation={ADD_COMMENT}
+					refetchQueries={() => {
+						return [{ query: GET_EVENT_COMMENTS, variables: { id: event_ID } }];
+					}}
+				>
+					{(addComment, e) => (
+						<div className="input-group input-group-sm py-2 px-4">
+							<input
+								type="text"
+								className="form-control mx-0"
+								placeholder="Comment..."
+								onChange={this.onChange}
+								name="text"
+								value={text}
+								onKeyDown={e => this.createComment(e, user, event_ID, text, addComment)}
+							/>
+							<div className="input-group-append">
+								<Link
+									to="#"
+									className="btn bg-darkblue"
+									onClick={e => this.createComment(e, user, event_ID, text, addComment)}
+								>
+									<i className="fa fa-paper-plane text-white" aria-hidden="true" />
+								</Link>
+							</div>
+						</div>
+					)}
+				</Mutation>
+			</Fragment>
+		);
+	}
 }
 
 export default EventCommentFeedInput;
