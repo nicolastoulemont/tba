8a713f9d35f10f58957f0c7b68c01ad5f37fdf3d
Nicolas Toulemont <nicolahs.toulemont@gmail.com>
2019-03-31 19:00:53 +0200

    refactored the SBPanel from class based compo to func based compo using hooks

--- client/src/components/sidebar/sideBarPanel/index.js
+++ client/src/components/sidebar/sideBarPanel/index.js
@@ -1,71 +1,51 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
-
 import HEventsPanel from './hostedEvents/';
 import RegistrationPanel from './registrations';
 
-class SBPanel extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			registrationsDisplay: true
-		};
-	}
-
-	registrationsDisplay = e => {
-		this.setState({ registrationsDisplay: true });
-	};
-	userHostedEventsDisplay = e => {
-		this.setState({ registrationsDisplay: false });
-	};
-
-	render() {
-		const { user } = this.props;
-		const { registrationsDisplay } = this.state;
-		return (
-			<Fragment>
-				<div className="col pr-0">
-					<div className="ml-2 bg-white">
-						<div className="row m-0 p-0">
-							<div className="col border-right p-0 pt-1">
-								<Link
-									className="link-menu"
-									to="#"
-									onClick={this.registrationsDisplay}
-									data-togggle="tooltip"
-									data-placement="bottom"
-									title="Your registrations"
-								>
-									<h6 className="d-inline">Registrations</h6>
-								</Link>
-							</div>
-							<div className="col p-0 pt-1">
-								<Link
-									to="#"
-									className="link-menu"
-									onClick={this.userHostedEventsDisplay}
-									data-togggle="tooltip"
-									data-placement="bottom"
-									title="Your hosted events"
-								>
-									<h6 className="d-inline">Hosted events</h6>
-								</Link>
-							</div>
+export default function SBPanel({ user }) {
+	const [display, setDisplay] = useState('registrations');
+	return (
+		<Fragment>
+			<div className="col pr-0">
+				<div className="ml-2 bg-white">
+					<div className="row m-0 p-0">
+						<div className="col border-right p-0 pt-1">
+							<Link
+								className="link-menu"
+								to="#"
+								onClick={() => setDisplay('registrations')}
+								data-togggle="tooltip"
+								data-placement="bottom"
+								title="Your registrations"
+							>
+								<h6 className="d-inline">Registrations</h6>
+							</Link>
 						</div>
-						<div className="row  m-0 p-0">
-							<div className="mt-1 w-100">
-								{registrationsDisplay ? (
-									<RegistrationPanel user={user} />
-								) : (
-									<HEventsPanel user={user} />
-								)}
-							</div>
+						<div className="col p-0 pt-1">
+							<Link
+								to="#"
+								className="link-menu"
+								onClick={() => setDisplay('events')}
+								data-togggle="tooltip"
+								data-placement="bottom"
+								title="Your hosted events"
+							>
+								<h6 className="d-inline">Hosted events</h6>
+							</Link>
+						</div>
+					</div>
+					<div className="row  m-0 p-0">
+						<div className="mt-1 w-100">
+							{display === 'registrations' ? (
+								<RegistrationPanel user={user} />
+							) : (
+								<HEventsPanel user={user} />
+							)}
 						</div>
 					</div>
 				</div>
-			</Fragment>
-		);
-	}
+			</div>
+		</Fragment>
+	);
 }
-
-export default SBPanel;
