abfff989fba014abe833d126aebbc27515d9f137
Nicolas Toulemont <nicolahs.toulemont@gmail.com>
2019-04-11 12:11:33 +0200

    refactored the eventCommentFeedInput compo to a func compo with hooks

--- client/src/components/comments/EventCommentFeedInput.js
+++ client/src/components/comments/EventCommentFeedInput.js
@@ -1,23 +1,12 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Mutation } from 'react-apollo';
 import { ADD_COMMENT } from '../graphql/comment/Mutations';
 import { GET_EVENT_COMMENTS } from '../graphql/comment/Queries';
 
-class EventCommentFeedInput extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			text: ''
-		};
-	}
-
-	onChange = e => {
-		const { name, value } = e.target;
-		this.setState({ [name]: value });
-	};
-
-	createComment = (e, user, event_ID, text, addComment) => {
+const EventCommentFeedInput = ({ user, event_ID }) => {
+	const [text, setText] = useState('');
+	const createComment = (e, user, event_ID, text, addComment) => {
 		if (
 			(e.type === 'click' && e.target.className === 'fa fa-paper-plane text-white') ||
 			(e.type === 'keydown' && e.keyCode === 13)
@@ -26,48 +15,44 @@ class EventCommentFeedInput extends Component {
 			addComment({
 				variables: { user_ID: user, event_ID, text }
 			}).then(res => {
-				this.setState({ text: '' });
+				setText('');
 			});
 		}
 	};
 
-	render() {
-		const { user, event_ID } = this.props;
-		const { text } = this.state;
-		return (
-			<Fragment>
-				<Mutation
-					mutation={ADD_COMMENT}
-					refetchQueries={() => {
-						return [{ query: GET_EVENT_COMMENTS, variables: { id: event_ID } }];
-					}}
-				>
-					{(addComment, e) => (
-						<div className="input-group input-group-sm py-2 px-4">
-							<input
-								type="text"
-								className="form-control mx-0"
-								placeholder="Comment..."
-								onChange={this.onChange}
-								name="text"
-								value={text}
-								onKeyDown={e => this.createComment(e, user, event_ID, text, addComment)}
-							/>
-							<div className="input-group-append">
-								<Link
-									to="#"
-									className="btn bg-darkblue"
-									onClick={e => this.createComment(e, user, event_ID, text, addComment)}
-								>
-									<i className="fa fa-paper-plane text-white" aria-hidden="true" />
-								</Link>
-							</div>
+	return (
+		<Fragment>
+			<Mutation
+				mutation={ADD_COMMENT}
+				refetchQueries={() => {
+					return [{ query: GET_EVENT_COMMENTS, variables: { id: event_ID } }];
+				}}
+			>
+				{(addComment, e) => (
+					<div className="input-group input-group-sm py-2 px-4">
+						<input
+							type="text"
+							className="form-control mx-0"
+							placeholder="Comment..."
+							onChange={e => setText(e.target.value)}
+							name="text"
+							value={text}
+							onKeyDown={e => createComment(e, user, event_ID, text, addComment)}
+						/>
+						<div className="input-group-append">
+							<Link
+								to="#"
+								className="btn bg-darkblue"
+								onClick={e => createComment(e, user, event_ID, text, addComment)}
+							>
+								<i className="fa fa-paper-plane text-white" aria-hidden="true" />
+							</Link>
 						</div>
-					)}
-				</Mutation>
-			</Fragment>
-		);
-	}
-}
+					</div>
+				)}
+			</Mutation>
+		</Fragment>
+	);
+};
 
 export default EventCommentFeedInput;
