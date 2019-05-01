0ae4e3b23cc21d10afcb7c0b875856ca7b720011
Nicolas Toulemont <nicolahs.toulemont@gmail.com>
2019-05-01 17:12:12 +0200

    added regex check for the comment mutations

--- server/utils/comment/validation.js
+++ server/utils/comment/validation.js
@@ -6,13 +6,20 @@ const validateCommentInput = async data => {
 
 	data.text = !isEmpty(data.text) ? data.text : '';
 
-	if (!Validator.isLength(data.text, { min: 1, max: 250 })) {
-		let textLengthError = {
+	if (!Validator.isLength(data.text, { min: 1, max: 250 }))
+		errors.push({
 			path: 'text',
 			message: 'Your comment must be between 1 and 250 characters'
-		};
-		errors.push(textLengthError);
-	}
+		});
+
+	const ValidString = /^[\wáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ?!,€._-\s]+$/;
+	const stringRegExp = new RegExp(ValidString);
+
+	if (!data.text.match(stringRegExp))
+		errors.push({
+			path: 'text',
+			message: 'Only alphanumeric characters are accepted'
+		});
 
 	return {
 		errors,
